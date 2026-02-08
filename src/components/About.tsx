7ce353f31236e59f9c87b22d0cf50391c0894201
abhassomkuwar <abhassomkuwar7@gmail.com>
2026-02-09 01:57:41 +0530

    .

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
     const containerRef = useRef<HTMLDivElement>(null);
     const { scrollYProgress } = useScroll({
         target: containerRef,
-        offset: ["start end", "end start"]     sdhfsdh
+        offset: ["start end", "end start"]     
     });         
 
     const yHeader = useTransform(scrollYProgress, [0, 1], [0, -100]);
